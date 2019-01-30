c99d5d7532463f8989500657ac6b51b4629941d4
Aleksei Ivanchuk <325alex.000@gmail.com>
2019-01-30 20:33:09 +0200

    Update Tile Entity For Hatch`s

--- src/main/java/ua/lyohha/multiblockstorage/MultiBlockStorage.java
+++ src/main/java/ua/lyohha/multiblockstorage/MultiBlockStorage.java
@@ -22,7 +22,7 @@ public class MultiBlockStorage
 {
     public static final String modid = "multiblockstorage";
     public static final String modname = "MultiBlock Storage";
-    private static final int build = 2;
+    private static final int build = 3;
     public static final String version = "0.0.1." + build;
 
     //CreativeTab
